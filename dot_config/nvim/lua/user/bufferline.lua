778b05dcfd1f9f012dbbdbbc7fd87a1829ab5daf
Uros Karic <dot.kadza@gmail.com>
2022-07-17 20:56:01 +0200

    Initial commit

---
+++ dot_config/nvim/lua/user/bufferline.lua
@@ -0,0 +1,25 @@
+local status_ok, bufferline = pcall(require, "bufferline")
+if not status_ok then
+  return
+end
+
+bufferline.setup {
+  options = {
+    numbers = "none", -- | "ordinal" | "buffer_id" | "both" | function({ ordinal, id, lower, raise }): string,
+    close_command = "Bdelete! %d", -- can be a string | function, see "Mouse actions"
+    right_mouse_command = "Bdelete! %d", -- can be a string | function, see "Mouse actions"
+    left_mouse_command = "buffer %d", -- can be a string | function, see "Mouse actions"
+    middle_mouse_command = nil, -- can be a string | function, see "Mouse actions"
+    indicator_icon = "▎",
+    buffer_close_icon = "",
+    -- buffer_close_icon = '',
+    modified_icon = "●",
+    close_icon = "",
+    -- close_icon = '',
+    left_trunc_marker = "",
+    right_trunc_marker = "",
+    diagnostics = "nvim_lsp", -- | "nvim_lsp" | "coc",
+    diagnostics_update_in_insert = false,
+    offsets = { { filetype = "NvimTree", text = "", padding = 1 } },
+  },
+}
